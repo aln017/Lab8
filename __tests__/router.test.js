165862e484bdf3d5f57b340b65458aa76a96b778
aln017 <aln017@ucsd.edu>
2021-05-20 15:12:16 -0700

    part 1 jest testing, install puppeteer

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,26 @@
 /**
  * @jest-environment jsdom
  */
+ import { pushToHistory } from '../scripts/router.js';
 
+describe('current state is correct', () => {
+    test('settings is correct', () => {
+        expect(pushToHistory('settings').state.page).toBe('settings');
+    });
+
+    test('entry is correct', () => {
+        expect(pushToHistory('entry', 4).state.page).toBe('entry4');
+    });
+
+    test('default is correct', () => {
+        expect(pushToHistory('').state.page).toBe(undefined);
+    });
+});
+
+test('history length is correct', () => {
+    expect(pushToHistory('settings').length).toBe(5);
+});
+
+test('history length is correct', () => {
+    expect(pushToHistory('entry', 5).length).toBe(6);
+});
